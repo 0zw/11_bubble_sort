bd013ec3e0213895b0af7d963d9f6c673942d035
0zw <55938778+0zw@users.noreply.github.com>
2020-01-14 22:40:20 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[]) 
+#define NUMBER 100
+int main(int argc,char *argv[])
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
-
-    return 0;
+	int i,j,n,c,k;
+	int a[NUMBER];
+	for (i=0;i<NUMBER;i++) {
+		scanf_s("%d", &a[i]);
+		n = i;
+		if (a[i]==1)
+			break;
+	}
+	for (k = 0; k <= n; k++) {
+		printf("%d ", a[k]);
+	}
+	printf("\n");
+	for (i = 0; i < n;i++) {
+		for (j = n; j > i;j--) {
+			if (a[j - 1] > a[j]) {
+				int temp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = temp;
+			}
+			for (k = 0; k <= n; k++) {
+				printf("%d ", a[k]);
+			}
+			printf("\n");
+		}
+		
+		
+	}
+	return 0;
 }
