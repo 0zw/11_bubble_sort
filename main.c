27952e80a950c6472bf15e7b269efb0b5a0b843b
0zw <55938778+0zw@users.noreply.github.com>
2020-01-26 21:26:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,31 +3,31 @@
 #define NUMBER 100
 int main(int argc,char *argv[])
 {
-	int i=0,j,n,c,k;
+	int i=0,j,l,c,k;
 	char str[7];
-	int d;
-	d = atoi(str);
+	int n;
+	n = atoi(str);
 	 int a[NUMBER];
 	while (fgets(str, sizeof(str), stdin)) {
 	
-		a[i] = d;
-		n = i;
+		a[i] = n;
+		l = i;
 		if (a[i]==1)
 			break;
 	i++;
 	}	
-	for (k = 0; k <= n; k++) {
+	for (k = 0; k <= l; k++) {
 		printf("%d ", a[k]);
 	}
 	printf("\n");
-	for (i = 0; i < n;i++) {
-		for (j = n; j > i;j--) {
+	for (i = 0; i < l;i++) {
+		for (j = l; j > i;j--) {
 			if (a[j - 1] > a[j]) {
 				int temp = a[j];
 				a[j] = a[j - 1];
 				a[j - 1] = temp;
 			}
-			for (k = 0; k <= n; k++) {
+			for (k = 0; k <= l; k++) {
 				printf("%d ", a[k]);
 			}
 			printf("\n");
